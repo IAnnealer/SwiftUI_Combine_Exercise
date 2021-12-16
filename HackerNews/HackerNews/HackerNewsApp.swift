f5b2610bb2c511ebbbe0622ec354f3511117d657
ian <ian.yeo@kakaoenteprise.com>
2021-12-16 22:43:04 +0900

    Add HackerNews - Init project

---
+++ HackerNews/HackerNews/HackerNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsApp.swift
+//  HackerNews
+//
+//  Created by Ian on 2021/12/16.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
